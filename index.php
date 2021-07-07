8b950b017ff8649654c7f7c9d388cf0a0fc6aee4
mainulBUBT <shopniilmihan@gmail.com>
2021-07-07 19:47:51 +0600

    Update index.php
    just serialize

--- index.php
+++ index.php
@@ -1,59 +1,59 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="css/styles.css">
-    <link rel="stylesheet" type="text/css" href="css/indexs.css">
-     
-    
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/styles.css">
+  <link rel="stylesheet" type="text/css" href="css/indexs.css">
+  
+  
 </head>
 <body>
   <!-- Navigation bar -->
-      <nav class="navbar navbar-expand-lg navbar-dark">
-  <a class="navbar-brand" href="#">
-    <img src="images/icon1.png" width="40" height="40" class="d-inline-block align-top" alt="">Fast Delivery
-  </a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+  <nav class="navbar navbar-expand-lg navbar-dark">
+    <a class="navbar-brand" href="#">
+      <img src="images/icon1.png" width="40" height="40" class="d-inline-block align-top" alt="">Fast Delivery
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="user_login.php"><button type="button" class="btn btn-danger">Login</button></a>
-      </li>
-    </ul>
-  </div>
-</nav>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="user_login.php"><button type="button" class="btn btn-danger">Login</button></a>
+        </li>
+      </ul>
+    </div>
+  </nav>
 
 
-           
-<!-- Sub menu slidebar -->
+  
+  <!-- Sub menu slidebar -->
 
   <section id="banner"><div class="container"><div class="row">
-  <div class="col-sm-12 col-md-6 col-lg-6">
-    <p class="promo-title">BEST PARCEL DELIVERY AGENCY</p>
-    <p>Here in this tutorials you will learn how to make a website in HTML, CSS and Bootstrap, I'll show you how to create responsive modern looking website step by step with stylish section border.</p>
-  </div>
+    <div class="col-sm-12 col-md-6 col-lg-6">
+      <p class="promo-title">BEST PARCEL DELIVERY AGENCY</p>
+      <p>Here in this tutorials you will learn how to make a website in HTML, CSS and Bootstrap, I'll show you how to create responsive modern looking website step by step with stylish section border.</p>
+    </div>
 
-  <div class="col-sm-12 col-md-6 col-lg-6 text-center">
-    <img src="images/delivery1.png"  class="img-fluid">
-  </div>
-</div></div>
+    <div class="col-sm-12 col-md-6 col-lg-6 text-center">
+      <img src="images/delivery1.png"  class="img-fluid">
+    </div>
+  </div></div>
 
- <img src="images/wave2p.png" class="bottom-img img">
+  <img src="images/wave2p.png" class="bottom-img img">
 </section>
 
 
@@ -64,151 +64,151 @@
     <h1 class="title">What Can You Send?</h1>
     <div class="row text-center">
       <div class="col-sm-12 col-md-3 col-lg-3">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title d-flex justify-content-center">Gift</h5>
-        <img src="images/icon2.png" class="mx-auto d-block img-fluid img-thumbnail">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title d-flex justify-content-center">Gift</h5>
+            <img src="images/icon2.png" class="mx-auto d-block img-fluid img-thumbnail">
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
 
       <div class="col-sm-12 col-md-3 col-lg-3">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title d-flex justify-content-center">Document</h5>
-        <img src="images/icon3.png" class="mx-auto d-block img-fluid img-thumbnail">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title d-flex justify-content-center">Document</h5>
+            <img src="images/icon3.png" class="mx-auto d-block img-fluid img-thumbnail">
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
 
-  <div class="col-sm-12 col-md-3 col-lg-3">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title d-flex justify-content-center">Package</h5>
-        <img src="images/icon5.png" class="mx-auto d-block img-fluid img-thumbnail">
+      <div class="col-sm-12 col-md-3 col-lg-3">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title d-flex justify-content-center">Package</h5>
+            <img src="images/icon5.png" class="mx-auto d-block img-fluid img-thumbnail">
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
 
-<div class="col-sm-12 col-md-3 col-lg-3">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title d-flex justify-content-center">Accessories</h5>
-        <img src="images/icon6.png" class="mx-auto d-block img-fluid img-thumbnail">
+      <div class="col-sm-12 col-md-3 col-lg-3">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title d-flex justify-content-center">Accessories</h5>
+            <img src="images/icon6.png" class="mx-auto d-block img-fluid img-thumbnail">
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
     </div>
     <button type="button" class="btn btn-own mt-3">All Services</button>
-    </div>
+  </div>
 </section>
 <!------------------Services--------------->
 
-  <div class="container text-center bg-light">
-    <div class="row text-center">
-      <div class="col-sm-12 col-md-4 col-lg-4 services">
-        <img src="images/pathao1.png" class="service-img">
-        <h4>On Demand Delivery</h4>
-        <p>Left your charger or keys at home? Have it delivered to you on Pathao Parcel, without any hassle!</p>
-      </div>
-      <div class="col-sm-12 col-md-3 col-lg-3 services">
-        <img src="images/pathao2.png" class="service-img">
-        <h4>Trust Us</h4>
-        <p>You can trust us to deliver your most confidential documents to the desired place absolutely intact right on time!</p>
-      </div>
-      <div class="col-sm-12 col-md-4 col-lg-4 services">
-        <img src="images/pathao3.png" class="service-img">
-        <h4>Emergency? We Deliver Fast!</h4>
-        <p>With Fast Delivery, you can get your item in the quickest time. Because your emergencies are Parcel’s biggest concern!</p>
-      </div>
+<div class="container text-center bg-light">
+  <div class="row text-center">
+    <div class="col-sm-12 col-md-4 col-lg-4 services">
+      <img src="images/pathao1.png" class="service-img">
+      <h4>On Demand Delivery</h4>
+      <p>Left your charger or keys at home? Have it delivered to you on Pathao Parcel, without any hassle!</p>
     </div>
+    <div class="col-sm-12 col-md-3 col-lg-3 services">
+      <img src="images/pathao2.png" class="service-img">
+      <h4>Trust Us</h4>
+      <p>You can trust us to deliver your most confidential documents to the desired place absolutely intact right on time!</p>
+    </div>
+    <div class="col-sm-12 col-md-4 col-lg-4 services">
+      <img src="images/pathao3.png" class="service-img">
+      <h4>Emergency? We Deliver Fast!</h4>
+      <p>With Fast Delivery, you can get your item in the quickest time. Because your emergencies are Parcel’s biggest concern!</p>
+    </div>
+  </div>
 
-    </div>
+</div>
 
 
 <!------------------About Us--------------->
 <section id="about-us">
  <div class="container text-center">
-    <h1 class="title">Our Service Zone</h1>
-    <div class="row bg-light">
-      <div class="col-sm-12 col-md-6 col-lg-6">
-        <p class="about-title">Currently available in Dhaka Cities and Dhaka Zone</p>
-        <p>We are currently available in Dhaka Cities and Dhaka Zone. We are constantly expanding to more places! Parcel will cover more places soon.</p>
-      </div>
-      <div class="col-sm-12 col-md-6 col-lg-6">
-        <img src="images/delivery4.png" class="service-img">
-        
-      </div>
+  <h1 class="title">Our Service Zone</h1>
+  <div class="row bg-light">
+    <div class="col-sm-12 col-md-6 col-lg-6">
+      <p class="about-title">Currently available in Dhaka Cities and Dhaka Zone</p>
+      <p>We are currently available in Dhaka Cities and Dhaka Zone. We are constantly expanding to more places! Parcel will cover more places soon.</p>
+    </div>
+    <div class="col-sm-12 col-md-6 col-lg-6">
+      <img src="images/delivery4.png" class="service-img">
+      
     </div>
   </div>
+</div>
 
 
 </section>
 <!------------------Testimonial--------------->
 <section id="testimonial">
-   <div class="container text-center">
-    <h1 class="title">Team Members</h1>
-    <div class="row ">
-      <div class="col-sm-12 col-md-4 col-lg-4 d-flex justify-content-center mt-3">
+ <div class="container text-center">
+  <h1 class="title">Team Members</h1>
+  <div class="row ">
+    <div class="col-sm-12 col-md-4 col-lg-4 d-flex justify-content-center mt-3">
       <div class="card" style="width: 250px;">
        <img src='images/moinul.jpg' class='card-img-top img-responsive' alt='' style="hwidth:100%; height: 200px">
-           <div class="card-body">
-          <h5 class="card-title text-center">Shopnil Mihan</h5>
-          <p class="small">Front & Back-end Developer</p>
-            <ul class="follow-us clearfix">
-              <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-              <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-              <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-            </ul>
-        </div>
-         </div>
-    </div>
-
-    <div class="col-sm-12 col-md-4 col-lg-4 d-flex justify-content-center mt-3">
-    <div class="card" style="width: 250px;">
-       <img src='images/foysal.jpg' class='card-img-top img-responsive' alt='' style="hwidth:100%; height: 200px">
-           <div class="card-body">
-          <h5 class="card-title text-center">MD Foysal</h5>
-          <p class="small">Graphics Designer</p>
-            <ul class="follow-us clearfix">
-              <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-              <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-              <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-            </ul>
-        </div>
-         </div>
-    </div>
-
-     <div class="col-sm-12 col-md-4 col-lg-4 d-flex justify-content-center mt-3">
-    <div class="card" style="width: 250px;">
-       <img src='images/sima2.jpg' class='card-img-top img-responsive' alt='' style="width:100%; height: 200px">
-           <div class="card-body">
-          <h5 class="card-title text-center">Yesmine Sima</h5>
-          <p class="small">Back-end Developer</p>
-            <ul class="follow-us clearfix">
-              <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-              <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-              <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-            </ul>
-        </div>
-         </div>
+       <div class="card-body">
+        <h5 class="card-title text-center">Shopnil Mihan</h5>
+        <p class="small">Front & Back-end Developer</p>
+        <ul class="follow-us clearfix">
+          <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+          <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+          <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+        </ul>
+      </div>
     </div>
   </div>
+
+  <div class="col-sm-12 col-md-4 col-lg-4 d-flex justify-content-center mt-3">
+    <div class="card" style="width: 250px;">
+     <img src='images/foysal.jpg' class='card-img-top img-responsive' alt='' style="hwidth:100%; height: 200px">
+     <div class="card-body">
+      <h5 class="card-title text-center">MD Foysal</h5>
+      <p class="small">Graphics Designer</p>
+      <ul class="follow-us clearfix">
+        <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+        <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+        <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+<div class="col-sm-12 col-md-4 col-lg-4 d-flex justify-content-center mt-3">
+  <div class="card" style="width: 250px;">
+   <img src='images/sima2.jpg' class='card-img-top img-responsive' alt='' style="width:100%; height: 200px">
+   <div class="card-body">
+    <h5 class="card-title text-center">Yesmine Sima</h5>
+    <p class="small">Back-end Developer</p>
+    <ul class="follow-us clearfix">
+      <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+      <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+      <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+    </ul>
+  </div>
+</div>
+</div>
+</div>
 </div>
 
 </section>
 <!------------------Social Media--------------->
 <section id="social-media">
-   <div class="container text-center">
-    <p>FIND US ON SOCIAL MEDIA</p>
-    <div class="social-icons">
-      <a href="http://facebook.com/Shopnilmiha"><img src="images/facebook.png"></a>
-      <a href="http://facebook.com/Shopnilmiha"><img src="images/linkedin.png"></a>
-      <a href="http://facebook.com/Shopnilmiha"><img src="images/twitter.png"></a>
-      <a href="http://facebook.com/Shopnilmiha"><img src="images/instagram.png"></a>
-      <a href="http://facebook.com/Shopnilmiha"><img src="images/youtube.png"></a>
-    </div>
-    </div>
+ <div class="container text-center">
+  <p>FIND US ON SOCIAL MEDIA</p>
+  <div class="social-icons">
+    <a href="http://facebook.com/Shopnilmiha"><img src="images/facebook.png"></a>
+    <a href="http://facebook.com/Shopnilmiha"><img src="images/linkedin.png"></a>
+    <a href="http://facebook.com/Shopnilmiha"><img src="images/twitter.png"></a>
+    <a href="http://facebook.com/Shopnilmiha"><img src="images/instagram.png"></a>
+    <a href="http://facebook.com/Shopnilmiha"><img src="images/youtube.png"></a>
+  </div>
+</div>
 </section>
 
 <section id="footer">
